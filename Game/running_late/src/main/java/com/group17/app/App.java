c85efda3ba112cf8fcfdb1f43e20d1894809ec3c
kmarquez <kmarquez@sfu.ca>
2021-11-10 18:54:57 -0800

    Added frame/GUI to start off with

--- Game/running_late/src/main/java/com/group17/app/App.java
+++ Game/running_late/src/main/java/com/group17/app/App.java
@@ -4,10 +4,10 @@ package com.group17.app;
  * Hello world!
  *
  */
-public class App 
+public class App extends GUI
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new GUI();
     }
 }
--- Game/running_late/src/main/java/com/group17/app/GUI.java
+++ Game/running_late/src/main/java/com/group17/app/GUI.java
@@ -4,15 +4,33 @@ import java.awt.Canvas;
 
 import javax.swing.JFrame;
 
+import javax.swing.JPanel;
+
+import javax.swing.BorderFactory;
+
+import java.awt.GridLayout;
+
+
+
 public class GUI extends Canvas {
 
-    public GUI(int width, int height, String title){
-        
-        JFrame frame = new JFrame(title);
+    public GUI(){
 
+        
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(400, 400, 240, 240));
+        panel.setLayout(new GridLayout());
+
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setTitle("Running Late");
     }
+
    
 }
