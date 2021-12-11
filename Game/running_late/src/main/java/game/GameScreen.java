ac51964d9e30e469f033a7266fa0d88714f20075
jra81 <jra81@sfu.ca>
2021-12-10 16:45:26 -0800

    got rid of unnecessary condition in if statement

--- Game/running_late/src/main/java/game/GameScreen.java
+++ Game/running_late/src/main/java/game/GameScreen.java
@@ -299,7 +299,7 @@ public class GameScreen extends JPanel implements Runnable {
     public void drawBonusReward(Graphics2D G2D){
 
         if(counter > 60){
-            if(isBonusSet == false){
+            if(!isBonusSet){
                 aSetter.setBonusReward();
                 isBonusSet = true;
             }
@@ -308,7 +308,7 @@ public class GameScreen extends JPanel implements Runnable {
             }
             counter --;
         }
-        else if(counter <= 60 && counter > 0){
+        else if(counter > 0){
             obj[19] = null;
             counter --;
         }
